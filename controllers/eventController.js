eae38e76a671ee523a28cf2b6ff056700fface68
bot5294 <5294aqib@gmail.com>
2022-10-16 23:53:24 +0530

    Project ready

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -7,6 +7,7 @@ module.exports.createEvent = async (req, res) => {
     let {
       name,
       tagline,
+      attendees,
       schedule,
       description,
       moderator,
@@ -14,10 +15,6 @@ module.exports.createEvent = async (req, res) => {
       sub_category,
       rigor_rank,
     } = req.body;
-    // console.log("req.file\n");
-    // console.log(req.files[0].path);
-    // console.log("\n");
-    // console.log(req.body);
     let arr = [];
     arr = req.files;
     for (let i = 0; i < arr.length; i++) arr[i] = arr[i].path;
@@ -31,6 +28,7 @@ module.exports.createEvent = async (req, res) => {
           description,
           moderator,
           category,
+          attendees,
           sub_category,
           rigor_rank,
           photos: arr,
@@ -50,100 +48,120 @@ module.exports.createEvent = async (req, res) => {
     });
   } catch (err) {
     console.log(`Error at createEvent : ${err}`);
+    return res.status(500).json({
+      message: "Internal Server Error, Contact Admin",
+    });
   }
 };
 
 module.exports.updateEvent = (req, res) => {
   try {
     let id = req.params.id;
-    let {
-      name,
-      tagline,
-      schedule,
-      description,
-      moderator,
-      category,
-      sub_category,
-      rigor_rank,
-    } = req.body;
-    let arr = [];
-    arr = req.files;
-    for (let i = 0; i < arr.length; i++) arr[i] = arr[i].path;
-    let isPhotoProvided = arr.length > 0 ? true : false;
-    console.log("isPhotoProvided : ", isPhotoProvided);
-    if (isPhotoProvided) {
-      if (arr.length != 2) {
-        return res.status(401).json({
-          message: "Provide only 2 photos or none",
+    if (ObjectId.isValid(id)) {
+      let {
+        name,
+        tagline,
+        schedule,
+        description,
+        moderator,
+        category,
+        attendees,
+        sub_category,
+        rigor_rank,
+      } = req.body;
+      let arr = [];
+      arr = req.files;
+      for (let i = 0; i < arr.length; i++) arr[i] = arr[i].path;
+      let attendeesArr = [];
+      attendeesArr = attendees;
+      attendeesArr.forEach((element) => {
+        return ObjectId(element);
+      });
+      let isPhotoProvided = arr.length > 0 ? true : false;
+      console.log("isPhotoProvided : ", isPhotoProvided);
+      if (isPhotoProvided) {
+        if (arr.length != 2) {
+          return res.status(401).json({
+            message: "Provide only 2 photos or none",
+          });
+        }
+        connection(async function (err, client) {
+          let db = await client.db("DT_Nodejs_DB");
+          let record = await db.collection("events").findOneAndUpdate(
+            { _id: new ObjectId(id) },
+            {
+              $set: {
+                name,
+                tagline,
+                schedule,
+                description,
+                moderator,
+                category,
+                sub_category,
+                attendees: attendeesArr,
+                rigor_rank,
+                photos: arr,
+              },
+            }
+          );
+          if (record) {
+            let photoArr = [];
+            photoArr = record.value.photos;
+            for (let i = 0; i < photoArr.length; i++) {
+              let filename = photoArr[i];
+              if (fs.existsSync(filename)) {
+                fs.unlinkSync(filename);
+                console.log(`${filename} Deleted successfully`);
+              } else {
+                console.log(
+                  "file not exists, unable delete photos at updateEvent"
+                );
+              }
+            }
+            return res.status(200).json({
+              message: "This record updated successfully",
+              record: record,
+            });
+          }
+        });
+      } else {
+        connection(async function (err, client) {
+          let db = await client.db("DT_Nodejs_DB");
+          let record = await db.collection("events").findOneAndUpdate(
+            { _id: new ObjectId(id) },
+            {
+              $set: {
+                name,
+                tagline,
+                schedule,
+                description,
+                moderator,
+                category,
+                attendees: attendeesArr,
+                sub_category,
+                rigor_rank,
+              },
+            },
+            { returnDocument: "after" }
+          );
+          if (record) {
+            return res.status(200).json({
+              message: "Here is the updated record [no photo update]",
+              record: record,
+            });
+          }
         });
       }
-      connection(async function (err, client) {
-        let db = await client.db("DT_Nodejs_DB");
-        let record = await db.collection("events").findOneAndUpdate(
-          { _id: new ObjectId(id) },
-          {
-            $set: {
-              name,
-              tagline,
-              schedule,
-              description,
-              moderator,
-              category,
-              sub_category,
-              rigor_rank,
-              photos: arr,
-            },
-          }
-        );
-        if (record) {
-          let photoArr = [];
-          photoArr = record.value.photos;
-          for (let i = 0; i < photoArr.length; i++) {
-            let filename = photoArr[i];
-            if (fs.existsSync(filename)) {
-              fs.unlinkSync(filename);
-              console.log(`${filename} Deleted successfully`);
-            } else {
-              console.log(
-                "file not exists, unable delete photos at updateEvent"
-              );
-            }
-          }
-          return res.status(200).json({
-            message: "This record updated successfully",
-            record: record,
-          });
-        }
-      });
     } else {
-      connection(async function (err, client) {
-        let db = await client.db("DT_Nodejs_DB");
-        let record = await db.collection("events").findOneAndUpdate(
-          { _id: new ObjectId(id) },
-          {
-            $set: {
-              name,
-              tagline,
-              schedule,
-              description,
-              moderator,
-              category,
-              sub_category,
-              rigor_rank,
-            },
-          },
-          { returnDocument: "after" }
-        );
-        if (record) {
-          return res.status(200).json({
-            message: "Here is the updated record [no photo update]",
-            record: record,
-          });
-        }
+      return res.status(404).json({
+        message: "Provide legitimate id",
       });
     }
   } catch (err) {
     console.log(`Error at updateEvent : ${err}`);
+    return res.status(500).json({
+      message: "Internal Server Error, Contact Admin",
+    });
   }
 };
 
@@ -195,25 +213,77 @@ module.exports.deleteEvent = (req, res) => {
 module.exports.getEventDetails = (req, res) => {
   try {
     if (req.query.id) {
-      let event_id = req.query.id;
-      return res.status(200).json({
-        message: `Event id received at backend is ${event_id}`,
+      let id = req.query.id;
+      if (ObjectId.isValid(id)) {
+        connection(async function (err, client) {
+          let db = await client.db("DT_Nodejs_DB");
+          let record = await db
+            .collection("events")
+            .findOne({ _id: new ObjectId(id) });
+          if (record) {
+            return res.status(200).json({
+              message: `Here are the requested event details`,
+              event: record,
+            });
+          } else {
+            return res.status(404).json({
+              message: `No record found with this id`,
+            });
+          }
+        });
+      } else {
+        return res.status(404).json({
+          message: "Provide legitimate id",
+        });
+      }
+    } else {
+      let type = req.query.type;
+      let limit = parseInt(req.query.limit);
+      let page = parseInt(req.query.page);
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+      let howToSort;
+      switch (type) {
+        case "latest":
+          howToSort = 1;
+          break;
+        case "oldest":
+          howToSort = -1;
+          break;
+        default:
+          howToSort = 1;
+          break;
+      }
+      connection(async function (err, client) {
+        let db = await client.db("DT_Nodejs_DB");
+        let cursor = await db
+          .collection("events")
+          .find({}, { projection: { name: 1, schedule: 1 } })
+          .sort({ schedule: howToSort })
+          .skip(startIndex)
+          .limit(limit);
+        let recordCount = await cursor.count();
+        let isAvailable = recordCount > 0;
+        if (isAvailable) {
+          let arr = [];
+          await cursor.forEach((e) => {
+            arr.push(e);
+          });
+          return res.status(200).json({
+            message: `Here are the requested event details`,
+            event: arr,
+          });
+        } else {
+          return res.status(404).json({
+            message: `No record found`,
+          });
+        }
       });
     }
-    let type = req.query.type;
-    let limit = req.query.limit;
-    let page = req.query.page;
-    let data = {
-      type: type,
-      limit: limit,
-      page: page,
-    };
-    console.log(page);
-    data = JSON.stringify(data);
-    res.status(200).json({
-      message: `Event details received at backend are as follows : ${data}`,
-    });
   } catch (err) {
     console.log(`Error at  ${err}`);
+    return res.status(500).json({
+      message: "Internal Server Error, Contact Admin",
+    });
   }
 };
