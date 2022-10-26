95a69cef253a7c05c43baa8cd2e52276dc4dd869
bot5294 <5294aqib@gmail.com>
2022-10-27 00:30:08 +0530

    Removal of Async-await completed

--- routes/api/v3/index.js
+++ routes/api/v3/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
-  //   dest: "./uploads",
   destination: function (req, file, cb) {
     cb(null, "./uploads");
   },
